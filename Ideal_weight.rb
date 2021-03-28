20ce3178037385b0905011c1232e77284e82d0c5
dudnykmaksym <81459553+dudnykmaksym@users.noreply.github.com>
2021-03-28 23:33:55 +0300

    Corrected Ruby_item2
    Corrected

--- Ideal_weight.rb
+++ Ideal_weight.rb
@@ -1,13 +1,13 @@
-print "Введите ваше имя: "
-name= gets.chomp()
+print 'Введите ваше имя: '
+name = gets.chomp
 
-print "Укажите ваш рост: "
-input_height = gets.chomp().to_i
+print 'Укажите ваш рост: '
+input_height = gets.chomp.to_i
 
-print "Укажите ваш вес: "
-input_weight = gets.chomp().to_i
+print 'Укажите ваш вес: '
+input_weight = gets.chomp.to_i
 
-w = (input_height - 110)*1.15
+w = (input_height - 110) * 1.15
 
 if w < 0
   puts "Ваш вес идеальный"
